e010fd79d4fffe4b364e4439499429c4ef5a7cb8
Samyuk Yadav Banda <aronisam325@gmail.com>
2022-03-09 20:29:11 +0000

    Working on setting side of the page.

---
+++ app/src/main/java/uk/ac/tees/W9519946/chat/Setting_Info.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.W9519946.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Setting_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_info);
+    }
+}
